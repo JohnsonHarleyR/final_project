ba2343c41f29077157ff3db07bdf8d005cf81fb0
bailierich <bailie.richard@gmail.com>
2020-06-12 11:44:35 -0400

    affirmation api added

---
+++ src/main/java/co/grandcircus/FinalProject/AffirmationsApi/Affirmations.java
@@ -0,0 +1,21 @@
+package co.grandcircus.FinalProject.AffirmationsApi;
+
+public class Affirmations {
+
+	private String affirmation;
+
+	public String getAffirmation() {
+		return affirmation;
+	}
+
+	public void setAffirmation(String affirmation) {
+		this.affirmation = affirmation;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/co/grandcircus/FinalProject/AffirmationsApi/AffirmationsService.java
@@ -0,0 +1,25 @@
+package co.grandcircus.FinalProject.AffirmationsApi;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AffirmationsService {
+	private RestTemplate rest = new RestTemplate();
+	
+	public String getAffirmation() {
+		
+		String url = "https://www.affirmations.dev/";
+		
+		Affirmations response = rest.getForObject(url, Affirmations.class);
+		
+		String affirmation = response.getAffirmation();
+		
+		
+		return affirmation;
+		
+		
+		
+	}
+
+}
